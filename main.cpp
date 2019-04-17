67695406aca287965f42d96255015e259ad4dd89
xPGAvKx <paulkodolitsch@gmail.com>
2019-04-17 11:39:36 +0200

    Main.cpp: Added file access

--- main.cpp
+++ main.cpp
@@ -20,16 +20,16 @@ namespace Sep
   const std::string ERROR_WRONG_ARGUMENTS = "Usage: ./sworms <config-file>";
 }
 
-int main()//int argc, char* argv[])
+int main(int argc, char* argv[])
 {
-  //if(argc != 2)
-  //{
-  //  std::cout << Sep::ERROR_WRONG_ARGUMENTS << '\n';
-  //  return Sep::RETURN_ERROR_ARGUMENT;
-  //}
+  if(argc != 2)
+  {
+    std::cout << Sep::ERROR_WRONG_ARGUMENTS << '\n';
+    return Sep::RETURN_ERROR_ARGUMENT;
+  }
 
   Sep::Game game;
-  if(game.loadConfig(std::string("config.txt")))
+  if(game.loadConfig(std::string(argv[1])))
     return Sep::RETURN_ERROR_BAD_CONFIG;
 
   game.printMap();
