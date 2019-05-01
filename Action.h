67283adb8545e10237e2960c54d7240d5900234d
Christian Luginger <luginger@student.tugraz.at>
2019-05-01 21:57:13 +0200

    Comments + CS Action

--- Action.h
+++ Action.h
@@ -24,6 +24,9 @@ namespace Sep
       //------------------------------------------------------------------------
       // Method
       // used to execute the command
+      //@param game  describes the current board
+      //@param params describes the vector where the input is stored
+      //
       int execute(Game& game, std::vector<std::string>& params) override;
 
       //------------------------------------------------------------------------
@@ -44,13 +47,45 @@ namespace Sep
       //
       int current_worm_;
 
+      //------------------------------------------------------------------------
+      //checkOneParameterCommand Method
+      //the funtion checks the param for Melee, and makes damage calculations
+      //@param game   describes the current board
+      //@param current_weapon   is the number of the current weapon
+      //
       bool checkOneParameterCommand(Game& game, int current_weapon);
 
+      //------------------------------------------------------------------------
+      //checkTwoParameterCommand Method
+      //the funtion checks the param for gun, bazooka and blowtorch, furthermore
+      //it makes damage calculations
+      //@param game   describes the current board
+      //@param current_weapon   is the number of the current weapon
+      //@param params describes the vector where the input is stored
+      //
       bool checkTwoParameterCommand(Game& game,
            std::vector<std::string>& params, int current_weapon);
 
+      //------------------------------------------------------------------------
+      //checkAirstrikeCommand
+      //the funtion checks the current weapon for AIRSTRIKE, furthermore
+      //it makes damage calculations
+      //@param game   describes the current board
+      //@param current_weapon   is the number of the current weapon
+      //@param params describes the vector where the input is stored
+      //
+
       bool checkAirstrikeCommand(Game& game,
            std::vector<std::string>& params, int current_weapon);
+
+      //------------------------------------------------------------------------
+      //checkTwoParameterCommand Method
+      //the funtion checks the param for gun, bazooka and blowtorch, furthermore
+      //it makes damage calculations
+      //@param game   describes the current board
+      //@param current_weapon   is the number of the current weapon
+      //@param params describes the vector where the input is stored
+      //
 
       bool checkThreeParameterCommand(Game& game,
            std::vector<std::string>& params, int current_weapon);
