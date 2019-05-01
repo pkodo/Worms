f9ba179ea2134f0bd01d7fd85f7d315c64469f04
Christian Luginger <luginger@student.tugraz.at>
2019-05-01 22:57:05 +0200

    CS + Comments

--- Weapon.h
+++ Weapon.h
@@ -16,27 +16,39 @@ namespace Sep
   class Weapon
   {
     public:
+      //Constructor
       Weapon();
-
+      //Constructor
       Weapon(const std::string &name, int ammo, int damage);
-
+      //Destructor
       virtual ~Weapon();
-
+      //Getter Method
+      //get ammunition
       int getAmmo() const;
 
+      //decreaseAmmo Method
+      //decrease the ammunition
       void decreaseAmmo();
 
+      //increaseAmmo Method
+      //increase the ammunition
       void increaseAmmo();
 
+      //Getter Method
+      //gets the name of the weapen
       const std::string &getName() const;
 
+      //getDamage Method
+      //Calculates the Damage
       int getDamage() const;
-
       virtual void action() const;
 
     private:
+      //Variable for the name of the weapon
       std::string name_;
+      //Variable for the ammunition of the weapon
       int ammo_;
+      //Variable for the damage of the weapon
       int damage_;
   };
 }
